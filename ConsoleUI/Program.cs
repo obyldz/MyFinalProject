31e2063af25ab4c08934d41b7d55deb04e3a23ea
obyldz <omerb.yildizz@gmail.com>
2026-08-06 17:05:04 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetAllByCategory(2))
+{
+    Console.WriteLine(product.ProductName);
+}
+
+
