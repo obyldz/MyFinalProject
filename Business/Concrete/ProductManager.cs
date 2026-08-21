e1c6ad707567f464ed5f4b66258e51ab17021098
obyldz <omerb.yildizz@gmail.com>
2026-08-21 16:44:52 +0300

    11.2

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 17)
+            if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>((Messages.MaintenanceTime));
             }
